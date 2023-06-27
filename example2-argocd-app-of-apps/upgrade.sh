422152424f6aa71a79abecdc3833d45bedf84173
shawn_campbell <drgeektechprof@gmail.com>
2023-06-27 13:22:27 -0400

    example2 upgrade.sh

--- example2-argocd-app-of-apps/upgrade.sh
+++ example2-argocd-app-of-apps/upgrade.sh
@@ -4,26 +4,41 @@
 set -e
 
 new_ver=$1
+image_owner="sbcampbe"
+image_base="nginx"
+image_base_tag="1.25.1"
+git_repo_url="git@github.com:sb-campbell/cicd-argocd-gitops-demo.git"
+base_folder="example2-argocd-app-of-apps"
 
-echo "new version: $new_ver"
+# array containing the resource .yaml files to be upgraded, space delimited list
+appsArray=("my-app-one/1-deployment.yaml" "my-app-two/1-deployment.yaml")
+
+#####################################
+# Main
+#####################################
+
+echo "new version: ${new_ver}"
 
 # Simulate release of the new docker images
-docker tag nginx:1.25.1 sbcampbe/nginx:$new_ver
+docker tag ${image_base}:${image_base_tag} ${image_owner}/${image_base}:${new_ver}
 
 # Push new version to dockerhub
-docker push sbcampbe/nginx:$new_ver
+docker push ${image_owner}/${image_base}:${new_ver}
 
 # Create temporary folder
 tmp_dir=$(mktemp -d)
-echo $tmp_dir
+echo "tmp_dir: ${tmp_dir}"
 
 # Clone GitHub repo
-git clone git@github.com:sb-campbell/cicd-argocd-gitops-demo.git $tmp_dir
+git clone ${git_repo_url} ${tmp_dir}
 
 # Update image tag
-# -i - update in place
-# -e - script, or following command treated as a script
-sed -i '' -e "s/sbcampbe\/nginx:.*/sbcampbe\/nginx:$new_ver/g" $tmp_dir/my-app/1-deployment.yaml
+for app_item in ${appsArray[@]}; do
+  # -i - update in place, 
+  # -e - script, or following command treated as a script
+  echo ${app_item}
+  sed -i '' -e "s/${image_owner}\/${image_base}:.*/${image_owner}\/${image_base}:${new_ver}/g" ${tmp_dir}/${base_folder}/${app_item}
+done
 
 # Commit and push
 cd $tmp_dir
